40eeae317ed03c449d6f90774ea61aaa646302a6
Jordi <jordilucas3@gmail.com>
2020-03-24 18:58:44 -0300

    Removido toolbar, adicionado imagem not found

--- app/src/main/java/com/jordilucas/coronanews/extensions/Picasso.kt
+++ app/src/main/java/com/jordilucas/coronanews/extensions/Picasso.kt
@@ -1,23 +1,29 @@
 package com.jordilucas.coronanews.extensions
 
+import android.graphics.BitmapFactory
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
+import com.jordilucas.coronanews.R
 import com.squareup.picasso.Callback
 import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_list.view.*
 import java.lang.Exception
 
 fun ImageView.loadUrl(url: String?, progress: ProgressBar? = null) {
     if(url == null || url.trim().isEmpty()){
-        setImageBitmap(null)
+        //Picasso.get().load(url).fit().centerCrop().placeholder(R.drawable.not_found).into(this)
+        setImageBitmap(
+            BitmapFactory.decodeResource(context.getResources(),
+            R.drawable.not_found))
         return
     }
     if(progress == null){
-        Picasso.get().load(url).fit().into(this)
+        Picasso.get().load(url).fit().centerCrop().placeholder(R.drawable.not_found).into(this)
     }
     else{
         progress.visibility = View.VISIBLE
-        Picasso.get().load(url).fit().into(this, object : Callback{
+        Picasso.get().load(url).fit().centerCrop().placeholder(R.drawable.not_found).into(this, object : Callback{
             override fun onSuccess() {
                 progress.visibility = View.GONE
             }
